28ca086ad2dc471fac0219b8244f8e7fb217a351
Ibrahim <i.alamin5@gmail.com>
2016-05-03 10:54:32 -0400

    added patient index

--- doctor/urls.py
+++ doctor/urls.py
@@ -9,4 +9,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^hello/$', views.index, name='hello')
 ]
--- patient/views.py
+++ patient/views.py
@@ -5,5 +5,7 @@ from django.http import HttpResponse
 # 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the patient index.")
+    #return HttpResponse("Hello, world. You're at the patient index.")
+
+    return render(request, 'patient/index.html')
 
