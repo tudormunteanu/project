6b5862e2d04dedb2ff1155938e0ce8400f574dcf
Ibrahim <i.alamin5@gmail.com>
2016-05-03 19:32:36 -0400

    Continue work on views

--- doctor/views.py
+++ doctor/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Doctor
 
 
 
 def index(request):
     #return HttpResponse("Hello, world. You're at the doctor index.")
+
+    #return render(request, 'doctor/index.html')
+
+    doctor_list = Doctor.objects.order_by('-last_name')
+
+    context = {
+
+		 'doctor_list': doctor_list,
+	}
+
+
 
     return render(request, 'doctor/index.html')
 
